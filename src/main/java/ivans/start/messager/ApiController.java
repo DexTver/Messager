9fd632b3c9aba1c2178062ea5d33516be20ab13f
ivan <ivan.sharapov.05@mail.ru>
2022-11-08 18:42:02 +0300

    New api functions
    tak skazat: "chetverochka est"

--- src/main/java/ivans/start/messager/ApiController.java
+++ src/main/java/ivans/start/messager/ApiController.java
@@ -1,5 +1,6 @@
 package ivans.start.messager;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -12,29 +13,43 @@ public class ApiController {
     private final java.util.List<String> messages = new ArrayList<>();
 
     // curl localhost:8080/messages
+    // curl localhost:8080/messages?prefix=aba
     @GetMapping("messages")
-    public ResponseEntity<java.util.List<String>> getMessages() {
-        return ResponseEntity.ok(messages);
+    public ResponseEntity<java.util.List<String>> getMessages(
+            @RequestParam(value = "prefix", required = false) String prefix) {
+        if (prefix == null) {
+            return ResponseEntity.ok(messages);
+        }
+        java.util.List<String> prefixMessages = new ArrayList<>();
+        for (String message : messages) {
+            if (message.startsWith(prefix)) {
+                prefixMessages.add(message);
+            }
+        }
+        return ResponseEntity.ok(prefixMessages);
     }
 
     // curl -X POST localhost:8080/messages -H "Content-Type: text/plain" -d "ababa"
+    // curl -X POST localhost:8080/messages -H "Content-Type: text/plain" -d "abrba"
+    // curl -X POST localhost:8080/messages -H "Content-Type: text/plain" -d "abaca"
     @PostMapping("messages")
-    public ResponseEntity<Void> addMessage(@RequestBody String text) {
+    public ResponseEntity<Void> addMessage(
+            @RequestBody String text) {
         messages.add(text);
         return ResponseEntity.accepted().build();
     }
 
     // curl localhost:8080/messages/0
     @GetMapping("messages/{index}")
-    public ResponseEntity<String> getMessage(@PathVariable("index") Integer
-                                                     index) {
+    public ResponseEntity<String> getMessage(
+            @PathVariable("index") Integer index) {
         return ResponseEntity.ok(messages.get(index));
     }
 
     // curl -X DELETE localhost:8080/messages/0
     @DeleteMapping("messages/{index}")
-    public ResponseEntity<Void> deleteText(@PathVariable("index") Integer
-                                                   index) {
+    public ResponseEntity<Void> deleteText(
+            @PathVariable("index") Integer index) {
         messages.remove((int) index);
         return ResponseEntity.noContent().build();
     }
@@ -48,4 +63,48 @@ public class ApiController {
         messages.add(i, message);
         return ResponseEntity.accepted().build();
     }
+
+    // curl localhost:8080/messages/search/aba
+    @GetMapping("messages/search/{text}")
+    public ResponseEntity<Integer> getMessageIndex(
+            @PathVariable("text") String text) {
+        for (int i = 0; i < messages.size(); i++) {
+            if (messages.get(i).startsWith(text)) {
+                return ResponseEntity.ok(i);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    // curl localhost:8080/messages/count
+    @GetMapping("messages/count")
+    public ResponseEntity<Integer> getCount() {
+        return ResponseEntity.ok(messages.size());
+    }
+
+    // curl -X POST localhost:8080/messages/2/create -H "Content-Type: text/plain" -d "dbdbd"
+    @PostMapping("messages/{index}/create")
+    public ResponseEntity<Void> createMessage(
+            @PathVariable("index") Integer index,
+            @RequestBody String text) {
+        if (index < messages.size()) {
+            messages.add(index, text);
+        } else {
+            messages.add(text);
+        }
+        return ResponseEntity.accepted().build();
+    }
+
+    // curl -X DELETE localhost:8080/messages/search/cbc
+    @DeleteMapping("messages/search/{text}")
+    public ResponseEntity<Void> deleteMessages(
+            @PathVariable("text") String text) {
+        for (int i = 0; i < messages.size(); i++) {
+            if (messages.get(i).startsWith(text)) {
+                messages.remove(i);
+            }
+        }
+        return ResponseEntity.noContent().build();
+    }
+
 }
