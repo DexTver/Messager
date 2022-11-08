5e2f1f43f0f6ce82257f71645e1cd05defb86486
ivan <ivan.sharapov.05@mail.ru>
2022-11-08 17:31:16 +0300

    I'd written "curls"

--- src/main/java/ivans/start/messager/ApiController.java
+++ src/main/java/ivans/start/messager/ApiController.java
@@ -10,26 +10,36 @@ import java.util.ArrayList;
 @RestController
 public class ApiController {
     private final java.util.List<String> messages = new ArrayList<>();
+
+    // curl localhost:8080/messages
     @GetMapping("messages")
     public ResponseEntity<java.util.List<String>> getMessages() {
         return ResponseEntity.ok(messages);
     }
+
+    // curl -X POST localhost:8080/messages -H "Content-Type: text/plain" -d "ababa"
     @PostMapping("messages")
     public ResponseEntity<Void> addMessage(@RequestBody String text) {
         messages.add(text);
         return ResponseEntity.accepted().build();
     }
+
+    // curl localhost:8080/messages/0
     @GetMapping("messages/{index}")
     public ResponseEntity<String> getMessage(@PathVariable("index") Integer
                                                      index) {
         return ResponseEntity.ok(messages.get(index));
     }
+
+    // curl -X DELETE localhost:8080/messages/0
     @DeleteMapping("messages/{index}")
     public ResponseEntity<Void> deleteText(@PathVariable("index") Integer
                                                    index) {
         messages.remove((int) index);
         return ResponseEntity.noContent().build();
     }
+
+    // curl -X PUT localhost:8080/messages/0 -H "Content-Type: text/plain" -d "cbcbc"
     @PutMapping("messages/{index}")
     public ResponseEntity<Void> updateMessage(
             @PathVariable("index") Integer i,
